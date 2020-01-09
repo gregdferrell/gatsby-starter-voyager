56702943e005a846f5eac0ecf262a714e3f66da8
Greg Ferrell <gferrell20@gmail.com>
2020-01-09 17:00:53 -0400

    add tina to project & gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -188,5 +188,15 @@ module.exports = {
         precachePages: ["", "/blog", "/about"],
       },
     },
+    {
+      resolve: "gatsby-plugin-tinacms",
+      options: {
+        sidebar: {
+          hidden: process.env.NODE_ENV === "production",
+          position: "displace",
+        },
+        plugins: ["gatsby-tinacms-git", "gatsby-tinacms-remark"],
+      },
+    },
   ],
 }
